6d17b218a2d81e04de21242c304bf5a33f56aa87
Abhishek Patil <abishkpatil@gmail.com>
2024-05-17 17:23:39 +0530

    feat: added carousel

--- src/customer/components/HomeCarosel/MainCrosel.jsx
+++ src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
-import { mainCarouselData } from './MainCaroselData'
-import AliceCarousel from 'react-alice-carousel'
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
+//images
+import img1 from "../../../image/Firstpage.jpg"
+import img2 from "../../../image/brushes.jpg"
+import img3 from "../../../image/sofapaper.jpg"
+import img4 from "../../../image/wallpaper.jpg"
 
-const MainCrosel = () => {
+const responsive = {
+    0: { items: 1 },
+    568: { items: 1 },
+    1024: { items: 1 },
+};
 
-      const items = mainCarouselData.map((item)=> <img className='cursor-pointer' role='presentation' src={item.image} alt=""/>)
+const items = [
+    <img className="item" src={img1} alt="image1" data-value="1"/>,
+    <img className="item" src={img2} alt="image1" data-value="2"/>,
+    <img className="item" src={img3} alt="image1" data-value="3"/>,
+    <img className="item" src={img4} alt="image1" data-value="4"/>,
+];
 
-    return (
-        <AliceCarousel
+const Carousel = () => (
+    <AliceCarousel
+        mouseTracking
         items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
     />
-    )
-}
+);
 
-export default MainCrosel
+export default Carousel;
+
+//I have commented your old code below
+
+// import React from 'react'
+// import { mainCarouselData } from './MainCaroselData'
+// import AliceCarousel from 'react-alice-carousel'
+// import 'react-alice-carousel/lib/alice-carousel.css';
+
+// //spelling yeno carousel da? :)
+// const MainCrosel = () => {
+
+//     const items = mainCarouselData.map((item) => <img className='cursor-pointer' role='presentation' src={item.image} alt="" />)
+
+//     return (
+//         <AliceCarousel
+//             items={items}
+//         />
+//     )
+// }
+
+// export default MainCrosel
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import MainCrosel from '../../components/HomeCarosel/MainCrosel'
 
-//you need to import the image like this, you can use any variable name, I've used *img1*
-import img1 from "../../../image/Firstpage.jpg"
-
 const Homepage = () => {
     return (
         <div>
             <MainCrosel />
-
-            {/* then in <img> tag, in the src attribute use the variable name  */}
-            <img src={img1} alt="" />
             <div>
                 other section
             </div>
