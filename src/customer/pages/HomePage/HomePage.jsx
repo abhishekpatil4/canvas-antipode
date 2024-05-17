fc6850dc87859cb2602d567d4fa9ed1fa987a642
Abhishek Patil <abishkpatil@gmail.com>
2024-05-17 17:02:22 +0530

    feat: import and add img

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import MainCrosel from '../../components/HomeCarosel/MainCrosel'
 
+//you need to import the image like this, you can use any variable name, I've used *img1*
+import img1 from "../../../image/Firstpage.jpg"
+
 const Homepage = () => {
     return (
         <div>
-               <MainCrosel/>
+            <MainCrosel />
 
-               <div>
+            {/* then in <img> tag, in the src attribute use the variable name  */}
+            <img src={img1} alt="" />
+            <div>
                 other section
-               </div>
+            </div>
 
         </div>
     )
